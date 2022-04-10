00356e0dbcf4309f3360482ea9941bb267d2c60d
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2022-04-10 14:55:35 -0300

    Controller Layer

--- design-patterns-spring/src/main/java/com/guilherme/designpatternsspring/service/ViaCepService.java
+++ design-patterns-spring/src/main/java/com/guilherme/designpatternsspring/service/ViaCepService.java
@@ -2,6 +2,7 @@ package com.guilherme.designpatternsspring.service;
 
 import com.guilherme.designpatternsspring.model.Address;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+    @GetMapping("/{cep}/json/")
     Address consultarCep(@PathVariable("cep") String cep);
 
 
